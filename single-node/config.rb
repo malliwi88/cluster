2c9354d852f03ae520ffb9e16e1344d2b07eeb53
Ben Cessa <ben@pixative.com>
2015-05-11 10:26:28 -0400

    NTP
    - Agrega la soporte para zona horaria y NTP
    - Decodifica archivos .bashrc
    - Configuracion manual de SSH

--- single-node/config.rb
+++ single-node/config.rb
@@ -5,12 +5,12 @@ $new_discovery_url='https://discovery.etcd.io/new'
 if File.exists?('user-data') && ARGV[0].eql?('up')
   require 'open-uri'
   require 'yaml'
- 
+  
   token = open($new_discovery_url).read
- 
+  
   data = YAML.load(IO.readlines('user-data')[1..-1].join)
   data['coreos']['etcd']['discovery'] = token
- 
+  
   yaml = YAML.dump(data)
   File.open('user-data', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
 end
