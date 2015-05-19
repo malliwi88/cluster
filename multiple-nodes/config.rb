ef6f8acdcec65afbc411f4008d2027c71c055ccc
Ben Cessa <ben@pixative.com>
2015-05-19 16:45:50 -0400

    Alpha Channel
    - Ajustes menores a la configuracion multinodos
    - Agrega canal 'alpha' por default
    - Actualiza version del user-data usado

--- multiple-nodes/config.rb
+++ multiple-nodes/config.rb
@@ -5,24 +5,24 @@ $new_discovery_url='https://discovery.etcd.io/new'
 if File.exists?('user-data') && ARGV[0].eql?('up')
   require 'open-uri'
   require 'yaml'
- 
+  
   token = open($new_discovery_url).read
- 
+  
   data = YAML.load(IO.readlines('user-data')[1..-1].join)
   data['coreos']['etcd']['discovery'] = token
- 
+  
   yaml = YAML.dump(data)
   File.open('user-data', 'w') { |file| file.write("#cloud-config\n\n#{yaml}") }
 end
 
-# Single instance cluster
+# Multiple instances cluster
 $num_instances=3
 
 # Basename of the VM
 $instance_name_prefix="core"
 
 # CoreOS channel
-$update_channel='stable'
+$update_channel='alpha'
 
 # Enable port forwarding of Docker TCP socket
 $expose_docker_tcp=2375
